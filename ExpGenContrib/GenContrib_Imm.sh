e1238e821b45c9456c466714565d36c139449cfe
Felix Beaudry <felix.beaudry@gmail.com>
2021-08-19 12:13:52 -0400

    update

--- ExpGenContrib/GenContrib_Imm.sh
+++ ExpGenContrib/GenContrib_Imm.sh
@@ -11,22 +11,18 @@ R -f GenContrib_Imm_input.R --vanilla
 #run gene dropping
 #if you haven't already compiled geneDrop_final.c, compile now: gcc geneDrop_final.c -lgsl
 python geneDrop_runner_RD.py working_files/intermediate_files/ImmContribAll_malevsfemale.ped working_files/intermediate_files/allABSnestlings.txt working_files/intermediate_files/ImmContribAll_malevsfemale_A 1000000 0 s A
-python geneDrop_runner_RD.py ImmContribYearly_malevsfemale.ped allABSnestlings.txt ImmContribYearly_malevsfemale_A 1000000 0 s A
+python geneDrop_runner_RD.py working_files/intermediate_files/ImmContribYearly_malevsfemale.ped working_files/intermediate_files/allABSnestlings.txt working_files/intermediate_files/ImmContribYearly_malevsfemale_A 1000000 0 s A
 
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt ImmContribAll_malevsfemale_Z 1000000 0 s Z
-python geneDrop_runner_RD.py ImmContribYearly_malevsfemale.ped allABSnestlings.txt ImmContribYearly_malevsfemale_Z 1000000 0 s Z
+python geneDrop_runner_RD.py working_files/intermediate_files/ImmContribAll_malevsfemale.ped working_files/intermediate_files/allABSnestlings.txt working_files/intermediate_files/ImmContribAll_malevsfemale_Z 1000000 0 s Z
+python geneDrop_runner_RD.py working_files/intermediate_files/ImmContribYearly_malevsfemale.ped working_files/intermediate_files/allABSnestlings.txt working_files/intermediate_files/ImmContribYearly_malevsfemale_Z 1000000 0 s Z
 
 
 ##assigning unsexed individuals a sex, with varying sex ratio
 
-#run gene dropping for Z & autosomes with proportion male 1 (all unsexed individuals assigned male)
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_1_Z 1000000 0 s Z 1
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_1_A 1000000 0 s A 1
-
-#run gene dropping for Z & autosomes with proportion male 0 (all unsexed individuals assigned female)
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_0_Z 1000000 0 s Z 0
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_0_A 1000000 0 s A 0
-
-#run gene dropping for Z & autosomes with proportion male 1 (all unsexed individuals assigned male)
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_0.48_Z 1000000 0 s Z 0.48
-python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_0.48_A 1000000 0 s A 0.48
+for sexRatio in 0 1 0.48
+do
+    for chrom in A Z 
+    do
+        python geneDrop_runner_RD.py ImmContribAll_malevsfemale.ped allABSnestlings.txt test_ImmContribAll_prop_male_${sexRatio}_${chrom} 1000000 0 s ${chrom} ${sexRatio}
+    done
+don
