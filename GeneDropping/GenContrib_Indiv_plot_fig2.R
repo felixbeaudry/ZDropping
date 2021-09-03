7438486d2c9c4efed358880652b2de89a0bfe22b
rose-driscoll <rdrisco2@ur.rochester.edu>
2021-09-03 16:28:44 -0400

    new plots with indiv contribs

--- GeneDropping/GenContrib_Indiv_plot_fig2.R
+++ GeneDropping/GenContrib_Indiv_plot_fig2.R
@@ -137,5 +137,216 @@ plot_grid(A_contribs_vs_descendants, Z_contribs_vs_descendants, A_vs_Z_contribs,
 #ggsave('fig2_EGC_AZ_6x2.pdf', width = 12, height = 4, units ='in')
 
 
+## Plot of offspring sex ratio vs. Z contribution for each mom
+
+# read in pedigree
+pedigree <- read.table("working_files/pedigree.txt", header = TRUE, stringsAsFactors = FALSE)
+
+# squish pedigree to get sex ratio of moms' offspring, then combine with indiv contribs
+# get rid of entries where mom is unknown
+indiv_contribs_offspring_sex_ratio <- filter(pedigree, Mom != "0") %>%
+  # group by mom and sex
+  group_by(Mom, Sex) %>%
+  # get number of offspring of each sex for each mom
+  dplyr::summarize(num_offspring = n()) %>%
+  # remove unsexed offspring as we can't do anything with these
+  filter(Sex != 0) %>%
+  # make sons and daughters into separate columns so that we can work with them
+  pivot_wider(id_cols = Mom, names_from = Sex, names_prefix = "num_offspring_", values_from = num_offspring) %>%
+  # calculate sex ratio: prop male offspring
+  mutate(prop_males = num_offspring_1/(num_offspring_1+num_offspring_2)) %>%
+  # pull out just the moms' IDs and sex ratios
+  select(Indiv = Mom, prop_males) %>%
+  # combine with indiv contribs data
+  left_join(indiv_contribs_prop_nestlings_2013, .)
+
+# plot
+ggplot(indiv_contribs_offspring_sex_ratio, aes(x = prop_males, y = Z_mean, color = Sex, fill = Sex)) +
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  scale_color_manual(values = c("cornflowerblue", "indianred1"))+
+  scale_fill_manual(values = c("cornflowerblue", "indianred1"))+
+  labs(x = "Offspring sex ratio (proportion male)", y = "Z expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt")) +
+  ylim(c(0,0.025)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.25, 0.33, 0.5, 0.66, 0.75, 1))
+
+# auto for comparison
+ggplot(indiv_contribs_offspring_sex_ratio, aes(x = prop_males, y = auto_mean, color = Sex, fill = Sex)) +
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  scale_color_manual(values = c("cornflowerblue", "indianred1"))+
+  scale_fill_manual(values = c("cornflowerblue", "indianred1"))+
+  labs(x = "Offspring sex ratio (proportion male)", y = "Autosomal expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt")) +
+  ylim(c(0,0.025)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.25, 0.33, 0.5, 0.66, 0.75, 1))
+
+# What about males
+indiv_contribs_offspring_sex_ratio_dads <- filter(pedigree, Dad != "0") %>%
+  # group by dad and sex
+  group_by(Dad, Sex) %>%
+  # get number of offspring of each sex for each dad
+  dplyr::summarize(num_offspring = n()) %>%
+  # remove unsexed offspring as we can't do anything with these
+  filter(Sex != 0) %>%
+  # make sons and daughters into separate columns so that we can work with them
+  pivot_wider(id_cols = Dad, names_from = Sex, names_prefix = "num_offspring_", values_from = num_offspring) %>%
+  # calculate sex ratio: prop male offspring
+  mutate(prop_males = num_offspring_1/(num_offspring_1+num_offspring_2)) %>%
+  # pull out just the moms' IDs and sex ratios
+  select(Indiv = Dad, prop_males) %>%
+  # combine with indiv contribs data
+  left_join(indiv_contribs_prop_nestlings_2013, .)
+
+# plot Z
+ggplot(indiv_contribs_offspring_sex_ratio_dads, aes(x = prop_males, y = Z_mean, color = Sex, fill = Sex)) +
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  scale_color_manual(values = c("cornflowerblue", "indianred1"))+
+  scale_fill_manual(values = c("cornflowerblue", "indianred1"))+
+  labs(x = "Offspring sex ratio (proportion male)", y = "Z expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt")) +
+  ylim(c(0,0.025)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.25, 0.33, 0.5, 0.66, 0.75, 1))
+
+# plot auto
+ggplot(indiv_contribs_offspring_sex_ratio_dads, aes(x = prop_males, y = auto_mean, color = Sex, fill = Sex)) +
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  scale_color_manual(values = c("cornflowerblue", "indianred1"))+
+  scale_fill_manual(values = c("cornflowerblue", "indianred1"))+
+  labs(x = "Offspring sex ratio (proportion male)", y = "Autosomal expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt")) +
+  ylim(c(0,0.025)) +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.25, 0.33, 0.5, 0.66, 0.75, 1))
 
 
+## Plot of male contrib vs. female contrib for all breeding pairs
+
+# squish pedigree to get breeding pairs, then combine with indiv contribs
+indiv_contribs_dad_vs_mom <- distinct(pedigree, Dad, Mom) %>%
+  filter(Dad != "0", Mom != "0") %>%
+  left_join(select(indiv_contribs_prop_nestlings_2013, Dad = Indiv, Dad_Z_mean = Z_mean, Dad_auto_mean = auto_mean)) %>%
+  left_join(select(indiv_contribs_prop_nestlings_2013, Mom = Indiv, Mom_Z_mean = Z_mean, Mom_auto_mean = auto_mean))
+
+# plot Z contribs
+ggplot(indiv_contribs_dad_vs_mom, aes(x = Dad_Z_mean, y = Mom_Z_mean)) +
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+  labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+
+# plot auto contribs for contrast
+ggplot(indiv_contribs_dad_vs_mom, aes(x = Dad_auto_mean, y = Mom_auto_mean)) +
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  geom_point(alpha = 0.75, size = 0.3)+
+  geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+  labs(x = "Male's autosomal expected genetic contrib. in 2013", y = "Female's autosomal expected genetic contrib. in 2013")+
+  plottheme+
+  theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+
+# how many different partners do you have
+dad_partners <- group_by(indiv_contribs_dad_vs_mom, Dad) %>%
+  dplyr::summarize(dad_partners_n = n())
+mom_partners <- group_by(indiv_contribs_dad_vs_mom, Mom) %>%
+  dplyr::summarize(mom_partners_n = n())
+indiv_contribs_dad_vs_mom_count_partners <- left_join(indiv_contribs_dad_vs_mom, dad_partners) %>%
+  left_join(mom_partners) %>%
+  mutate(partners_difference = dad_partners_n - mom_partners_n)
+
+# distribution of partner numbers
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x=dad_partners_n)) +
+  geom_histogram()
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x=mom_partners_n)) +
+  geom_histogram()
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x=partners_difference)) +
+  geom_histogram()
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x=dad_partners_n, y=mom_partners_n)) +
+  geom_point(alpha = 0.01)
+
+# pull out monogamous pairs
+monogamous_pairs <- filter(indiv_contribs_dad_vs_mom_count_partners, dad_partners_n == 1, mom_partners_n == 1)
+# plot
+ggplot(monogamous_pairs, aes(x = Dad_Z_mean, y = Mom_Z_mean)) +
+geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+geom_point(alpha = 0.75, size = 0.3)+
+geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+plottheme+
+theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+#auto
+ggplot(monogamous_pairs, aes(x = Dad_auto_mean, y = Mom_auto_mean)) +
+geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+geom_point(alpha = 0.75, size = 0.3)+
+geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+plottheme+
+theme(legend.position = "none", plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+
+# combine with sex ratio of offspring
+monogamous_pairs_offspring_sex_ratio <- left_join(monogamous_pairs, select(indiv_contribs_offspring_sex_ratio, Mom = Indiv, prop_males))
+# plot Z
+ggplot(monogamous_pairs_offspring_sex_ratio, aes(x = Dad_Z_mean, y = Mom_Z_mean)) +
+#geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+geom_point(aes(color = prop_males), alpha = 0.75, size = 1)+
+geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+plottheme+
+theme(plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+# plot auto
+ggplot(monogamous_pairs_offspring_sex_ratio, aes(x = Dad_auto_mean, y = Mom_auto_mean)) +
+#geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+geom_point(aes(color = prop_males), alpha = 0.75, size = 0.3)+
+geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+plottheme+
+theme(plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))
+# who are those 6 pairs on the Z
+filter(monogamous_pairs_offspring_sex_ratio, (Dad_Z_mean - Mom_Z_mean)>0.001)
+
+
+# male vs female contribs, now colored by difference in number of partners
+
+# plot Z
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x = Dad_Z_mean, y = Mom_Z_mean, color = partners_difference)) +
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  geom_point(alpha = 0.75, size = 1)+
+  geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+  labs(x = "Male's Z expected genetic contrib. in 2013", y = "Female's Z expected genetic contrib. in 2013")+
+  plottheme+
+  theme(plot.margin = unit(c(15,5.5,5.5,5.5), "pt")) +
+  scale_color_gradient2(low = "darkred", mid = "mediumpurple", high = "darkblue")
+# plot auto
+ggplot(indiv_contribs_dad_vs_mom_count_partners, aes(x = Dad_auto_mean, y = Mom_auto_mean, color = partners_difference)) +
+  geom_smooth(method = "lm", alpha = 0.3, size = 0.5)+
+  geom_point(alpha = 0.75, size = 1)+
+  geom_abline(slope = 1,  linetype = "dashed", color = "gray") +
+  labs(x = "Male's autosomal expected genetic contrib. in 2013", y = "Female's autosomal expected genetic contrib. in 2013")+
+  plottheme+
+  theme(plot.margin = unit(c(15,5.5,5.5,5.5), "pt"))+
+  scale_color_gradient2(low = "darkred", mid = "mediumpurple", high = "darkblue")
+
+# male-female contribs vs difference in number of partners
+indiv_contribs_dad_vs_mom_count_partners_exp_gen_contrib_diff <- mutate(indiv_contribs_dad_vs_mom_count_partners, Z_mean_diff = Dad_Z_mean - Mom_Z_mean, auto_mean_diff = Dad_auto_mean - Mom_auto_mean)
+# Z
+ggplot(indiv_contribs_dad_vs_mom_count_partners_exp_gen_contrib_diff, aes(x = partners_difference, y=Z_mean_diff)) +
+  geom_point()+
+  scale_y_continuous(limits = c(-0.02,0.03)) +
+  geom_smooth(method = "lm")
+# auto
+ggplot(indiv_contribs_dad_vs_mom_count_partners_exp_gen_contrib_diff, aes(x = partners_difference, y=auto_mean_diff)) +
+  geom_point() +
+  scale_y_continuous(limits = c(-0.02,0.03)) +
+  geom_smooth(method = "lm")
+
+# fixing some colors
+indiv_contribs_dad_vs_mom_count_partners$partners_difference_fct <- as.factor(indiv_contribs_dad_vs_mom_count_partners$partners_difference)
+
+## Felix, have fun!
