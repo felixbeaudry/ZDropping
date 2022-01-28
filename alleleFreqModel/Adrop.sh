09f517e95af278c1c9e668c066ad03a32fc85be2
Felix Beaudry <felix.beaudry@gmail.com>
2022-01-28 11:36:01 -0500

    email update

--- alleleFreqModel/Adrop.sh
+++ alleleFreqModel/Adrop.sh
@@ -3,13 +3,17 @@
 #SBATCH -c 20 --mem=200G  
 
 module load r/4.1.1/b1
+module load sendemail/1.56
+
 
 echo "A sample"
 Rscript --vanilla alleleFreqModelA_sample.R
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adropsample -m Adropsample
+
+
 echo "A sim"
 Rscript --vanilla alleleFreqModelA_sim.R
 #echo "A boot"
 #Rscript --vanilla alleleFreqModelA_boot.R
 
-module load sendemail/1.56
-sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adrop -m Adrop
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adropsim -m Adropsim
--- alleleFreqModel/Zdrop.sh
+++ alleleFreqModel/Zdrop.sh
@@ -6,10 +6,14 @@ module load r/4.1.1/b1
 
 echo "Z sample"
 Rscript --vanilla alleleFreqModelZ_sample.R
+
+module load sendemail/1.56
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zsample -m Zsample
+
 echo "Z sim"
 Rscript --vanilla alleleFreqModelZ_sim.R
 #echo "Z boot"
 #Rscript --vanilla alleleFreqModelZ_boot.R
 
-module load sendemail/1.56
-sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zdrop -m Zdrop
+
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zsim -m Zsim
