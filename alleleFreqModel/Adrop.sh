717468349120712509d9308251b8747c839cc3c4
Felix Beaudry <felix.beaudry@gmail.com>
2022-01-29 13:21:28 -0500

    versioning trouble

--- alleleFreqModel/Adrop.sh
+++ alleleFreqModel/Adrop.sh
@@ -2,9 +2,8 @@
 #SBATCH --partition=rosalind  --time=10-00:00:00  --output=Adrop.r220128.log
 #SBATCH -c 20 --mem=200G  
 
-module load r/4.1.1/b1
+module load R
 module load sendemail/1.56
-
 
 echo "A sample"
 Rscript --vanilla alleleFreqModelA_sample.R
@@ -13,7 +12,8 @@ sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adropsamp
 
 echo "A sim"
 Rscript --vanilla alleleFreqModelA_sim.R
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adropsim -m Adropsim
+
 #echo "A boot"
 #Rscript --vanilla alleleFreqModelA_boot.R
 
-sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Adropsim -m Adropsim
--- alleleFreqModel/Zdrop.sh
+++ alleleFreqModel/Zdrop.sh
@@ -2,18 +2,16 @@
 #SBATCH --partition=rosalind  --time=10-00:00:00  --output=Zdrop.r220128.log
 #SBATCH -c 20 --mem=200G 
 
-module load r/4.1.1/b1
+module load R
+module load sendemail/1.56
 
 echo "Z sample"
 Rscript --vanilla alleleFreqModelZ_sample.R
-
-module load sendemail/1.56
 sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zsample -m Zsample
 
 echo "Z sim"
 Rscript --vanilla alleleFreqModelZ_sim.R
+sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zsim -m Zsim
+
 #echo "Z boot"
 #Rscript --vanilla alleleFreqModelZ_boot.R
-
-
-sendEmail -f fbeaudry@ur.rochester.edu -t fbeaudry@ur.rochester.edu -u Zsim -m Zsim
