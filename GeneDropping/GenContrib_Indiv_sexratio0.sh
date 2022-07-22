d704b43a893c29712efe2c81fc75b6170cf50937
rose-driscoll <rdrisco2@ur.rochester.edu>
2022-07-22 11:47:26 -0400

    Add scripts to run indiv gen contribs w sex ratio 0, 0.5

---
+++ GeneDropping/GenContrib_Indiv_sexratio0.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#pipeline for estimating expected genetic contributions of 926 individual breeders, assigning all unsexed individuals as females
+#input files: pedigree.txt, 926_breeders.txt, & IndivDataUSFWS.txt
+#Nancy Chen, Rose Driscoll and Felix Beaudry
+#Last updated: 27 Apr 2022
+
+#generate ped files for the individuals
+#if this has already been done for GenContrib_Indiv.sh, no need to re-run
+#R -f GenContrib_Indiv_input.R --vanilla
+
+#run gene dropping sims - specify number of processors to parallelize over in nprocs
+#if you haven't already compiled geneDrop_RD.c, compile now: gcc geneDrop_RD.c -lgsl
+i=0
+nprocs=10 #number of processors to use
+
+for j in working_files/intermediate_files/IndivContrib_*.ped
+do
+    python geneDrop_runner_RD.py "$j" working_files/intermediate_files/allABSnestlings.txt "$j".A.0 1000000 0 s A 0 &
+    ((i++))
+    [[ $((i%nprocs)) -eq 0 ]] && wait
+done
+wait
+
+for j in working_files/intermediate_files/IndivContrib_*.ped
+do
+    python geneDrop_runner_RD.py "$j" working_files/intermediate_files/allABSnestlings.txt "$j".Z.0 1000000 0 s Z 0 &
+    ((i++))
+    [[ $((i%nprocs)) -eq 0 ]] && wait
+done
+wait
